b70519d6deb9fa8de3ecac92cb085f9937dbebd5
Vegito2367 <tejaaditya.g@gmail.com>
2024-03-20 00:46:08 -0500

    Added basic calculator functionality

---
+++ src/calculator.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main() {
+  int b1,b2,choice,output=0;
+  bool val1, val2;
+  cout << "Basic boolean calculator\n";
+  cout<<"Enter first boolean value: ";
+  cin>>b1;
+  cout<<"\nEnter second boolean value: ";
+  cin>>b2;
+  cout<<"\nEnter 1 for and, 2 for or, 3 xor, 4 for not: ";
+  cin>>choice;
+  val1=(b1==1)? true : false;
+  val2=(b2==1)? true : false;
+  switch (choice)
+  {
+  case 1: output=val1&val2;
+  break;
+  
+  case 2: output=val1|val2;
+  break;
+
+  case 3: output=(((!val1)&val2)|(val1&(!val2)));
+  break;
+  
+  case 4:
+    int c2;
+    cout<<"\nDo you want first or second not?(1/2)";
+    cin>>c2;
+    if(c2==1){
+      output=!val1;
+    }
+    else{
+      output=!val2;
+    }
+  default: cout<<"\nWrong choice!";
+    break;
+  }
+
+  char c1;
+  c1=(output)? 'T' : 'F';
+  cout<<"\nOutput for your choices is: "<<c1;
+
+  return 0;
+}
