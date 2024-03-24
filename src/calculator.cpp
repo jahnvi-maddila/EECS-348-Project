883ead7cbe71ecaebfc2425e8853beb0a4523760
Vegito2367 <tejaaditya.g@gmail.com>
2024-03-24 17:33:39 -0500

    Uploading Project docs

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <conio.h>
+#include <stack>
 using namespace std;
 int main() {
   int b1,b2,choice,output=0;
+  string expression;
   bool val1, val2;
   cout << "Basic boolean calculator\n";
-  cout<<"Enter first boolean value: ";
-  cin>>b1;
+  cout<<"Enter a boolean expression with T for 1 and F for 0\n";
+  cin>>expression;
   cout<<"\nEnter second boolean value: ";
   cin>>b2;
   cout<<"\nEnter 1 for and, 2 for or, 3 xor, 4 for not: ";
   cin>>choice;
   val1=(b1==1)? true : false;
   val2=(b2==1)? true : false;
+  
   switch (choice)
   {
   case 1: output=val1&val2;
